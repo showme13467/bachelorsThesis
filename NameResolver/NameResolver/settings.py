2774c433ba853d5d7da2eee6856395304e5c9e9a
Dario Schaumburger <56314314+showme13467@users.noreply.github.com>
2019-10-24 16:04:02 -0400

    update settings and image save

--- NameResolver/NameResolver/settings.py
+++ NameResolver/NameResolver/settings.py
@@ -77,16 +77,21 @@ WSGI_APPLICATION = 'NameResolver.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
+#DATABASES = {
+ #      'default': {
+  #         'ENGINE': 'djongo',
+   #        'NAME': 'cluster0',
+    #       'HOST' : 'mongodb+srv://root:didcfi13467@cluster0-urnne.mongodb.net/test?retryWrites=true&w=majority',
+     #      'USER' : 'root',
+      #     'PASSWORD' : 'didcfi13467',
+       # }
+  #  }
 DATABASES = {
-       'default': {
-           'ENGINE': 'djongo',
-           'NAME': 'cluster0',
-           'HOST' : 'mongodb+srv://root:didcfi13467@cluster0-urnne.mongodb.net/test?retryWrites=true&w=majority',
-           'USER' : 'root',
-           'PASSWORD' : 'didcfi13467',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- NameResolver/site_plan/models.py
+++ NameResolver/site_plan/models.py
@@ -24,3 +24,13 @@ class map(models.Model):
         
     def get_absolute_url(self):
         return reverse('table-detail', kwargs ={'pk': self.pk})
+
+    def save(self, *args, **kawrgs):
+        super.save(*args, **kawrgs)
+        
+        img = Image.open(self.image.path)
+        
+        if img.height > 500 or img.width > 500:
+            output_size = (500,500)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
