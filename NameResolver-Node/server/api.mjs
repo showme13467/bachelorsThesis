e1e1e690d96f0dd588043a7b1fc1ca73e523e4d3
Dario Schaumburger <dari-ds@t-online.de>
2019-11-26 17:54:36 -0500

    requests for create floor, device, room, building

--- NameResolver-Node/server/api.mjs
+++ NameResolver-Node/server/api.mjs
@@ -25,9 +25,6 @@ api.get('/get_devices', (req, res) => {
 
 });
 
-api.get('/get_rooms', (req, res) => {
-
-});
 
 api.get('/get_floors', (req, res) => {
 
@@ -70,32 +67,202 @@ api.get('/back', (req, res) => {
 
 // api to handle all post requests
 api.post('/create_device/', (req, res) => {
-
-	console.log(req.body);
-	res.status(200).send();
+  console.log(req.body);
+ let col_name = db.collection('devices');
+col_name.findOne({$and:[{"name": req.body.name},{"room": req.body.room}]}, function(err,doc){
+if(err){
+console.log(err);
+res.send(error.message);
+}
+else{
+if(doc==null){
+insert_device(req,res);
+}
+else{
+console.log("Device already exists in that room");
+res.send(doc);
+}
+}
 });
+});
+
+function insert_device(req,res){
+let col_name = db.collection('devices');
+let obj = {"name": req.body.name, "type": req.body.type, "building": req.body.building, "floor": req.body.floor, "room": req.body.floor, "height": req.body.height, "url": req.body.url, "date_added": new Date() , "author": req.body.author, "image": req.body.image, "location": { "type": "Point", "coordinates": req.body.coordinates}};
+  col_name.insertOne(obj, function(error, result) {
+    if (error) {
+      console.error(error.message);
+      res.send(error.message);
+    } else {
+      update_room;
+      res.send(result);
+    }
+  });
+};
+
+function update_room(req,res){
+      let col_name = db.collection('rooms');
+      let room_name = req.body.room;
+      col_name.findOneAndUpdate({"name": room_name}, {$push: {devices: {[req.body.name]: res.ops[0]._id}}} , function(err, doc){
+        if(err){
+          console.log(err);
+}
+        else{
+          console.log(doc);
+}
+});
+};
 
 api.post('/create_floor/', (req, res) => {
-	console.log(req.body);
-        res.status(200).send();
-
+  console.log(req.body);
+ let col_name = db.collection('floors');
+col_name.findOne({"name": req.body.name}, function(err,doc){
+if(err){
+console.log(er);
+res.send(err.message);
+}
+else{
+if(doc==null){
+insert_floor(req,res);
+}
+else{
+console.log("Floor already exists!");
+res.send(doc);
+}
+}
 });
+});
+
+function insert_floor(req,res){
+let col_name = db.collection('floors');
+let obj = {"name": req.body.name, "coordinates": req.body.coordinates, "rooms":[] };
+  col_name.insertOne(obj, function(err, result) {
+    if (err) {
+      console.error(err.message);
+      res.send(err.message);
+    } else {
+      update_building(req,result);
+      res.send(result);
+    }
+  });
+};
+
+function update_building(req,res){
+      let col_name = db.collection('buildings');
+      let building_name = req.body.building;
+      col_name.findOneAndUpdate({"name": building_name}, {$push: {floors: {[req.body.name]: res.ops[0]._id}}} , function(err, doc){
+        if(err){
+          console.log(err);
+}
+        else{
+          console.log(doc);
+}
+});
+};
+
+
 
 api.post('/create_room/', (req, res) => {
-	console.log(req.body);
-        res.status(200).send();
-
+  console.log(req.body);
+    let col_name = db.collection('rooms');
+ col_name.findOne({"name": req.body.name}, function(err,doc){
+if(err){
+console.log(err);
+res.send(error.message);
+}
+else{
+if(doc==null){
+insert_room(req,res);
+}
+else{
+console.log("Room already exists!");
+res.send(doc);
+}
+}
 });
+});
+
+function insert_room(req,res){
+let col_name = db.collection('rooms');
+let obj = {"name": req.body.name, "coordinates": req.body.coordinates, "devices":[] }
+  col_name.insertOne(obj, function(error, result) {
+    if (error) {
+      console.error(error.message);
+      res.send(error.message);
+    } else {
+      update_floor(req,result);
+      res.send(result)
+    }
+  });
+};
+
+function update_floor(req,res){
+      let col_name = db.collection('floors');
+      let floor_name = req.body.floor;	
+      col_name.findOneAndUpdate({"name": floor_name}, {$push: {rooms: {[req.body.name]: res.ops[0]._id}}} , function(err, doc){
+        if(err){
+          console.log(err);
+}
+        else{
+          console.log(doc);
+}
+});
+};
+
+api.post('/get_rooms/', (req,res) => {
+  console.log(req.body);
+  let col_name = db.collection('buildings')
+  col_name.findOne({"name": req.body.building}, function(err, result){
+if(err){
+console.log(err);
+res.send(err);
+}
+else{
+console.log(result.floors);
+res.send(result.floors);
+}
+});
+});
+
+
+
 
 api.post('/create_building/', (req, res) => {
-	console.log(req.body);
-        res.status(200).send();
-
+ console.log(req.body);
+ let col_name = db.collection('buildings');
+col_name.findOne({"name": req.body.name}, function(err,doc){
+if(err){
+console.log(err);
+res.send(error.message);
+}
+else{
+if(doc==null){
+insert_building(req,res);
+}
+else{
+console.log("Building already exists!");
+res.send(doc);
+}
+}
+});
 });
 
+function insert_building(req,res){
+let col_name = db.collection('buildings');
+let obj = {"name": req.body.name, "floors":[] }
+  col_name.insertOne(obj, function(error, result) {
+    if (error) {
+      console.error(error.message);
+      res.send(error.message);
+    } else {
+      res.send(result)
+    }
+  });
+};
+
 api.post('/create_user/', (req, res) => {
-	console.log(req.body);
-        res.status(200).send();
+  console.log(req.body);
+  res.send(req.body);
 
 });
 
