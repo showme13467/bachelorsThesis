aba9019a7ea9311f74016ac4cb43b6df94b20550
Alexander Nußbaum <alexandern1234@gmail.com>
2019-12-06 22:53:58 -0500

    stable version with update, delete and create functions

---
+++ BA01/src/main/java/Floorview.js
@@ -0,0 +1,11 @@
+window.open('', 'test', 'height=480,width=640', false);
+function showCoords(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var coor = "X coords: " + x + ", Y coords: " + y;
+    document.getElementById("demo").innerHTML = coor;
+}
+
+function clearCoor() {
+    document.getElementById("demo").innerHTML = "";
+}
